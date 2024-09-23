9163a632d72d899b721c515bc5efeb0635f50d97
humd1 <humdb@nycstudents.net>
2024-09-23 09:20:13 -0400

    dftgdf

--- code.py
+++ code.py
@@ -1,7 +1,17 @@
-bill = input("how was your service?")
-if bill == "good":
+bill = (input("enter your bill value"))
+
+service_quality: (input("How was your service? Bad, Good, Okay, or Great?"))
+
+if service_quality == "Good":
     print("Give a 20% tip?")
-elif bill == "bad":
+    print (bill * 1.2)
+elif service_quality == "Bad":
     print("No tip?")
-else:
+elif service_quality == "Okay":
     print("Give a 15% tip?")
+elif service_quality == "Great":
+    print("Give a 25% tip?")
+else: 
+    print("invalid input")
+
+bill()
