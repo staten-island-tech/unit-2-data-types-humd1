71e2b21aa39729e25da0b99ae24480c6399b5180
humd1 <humdb@nycstudents.net>
2024-09-30 08:47:06 -0400

    finished

--- factors.py
+++ factors.py
@@ -1,6 +1,10 @@
 x = int(input("Enter the first number to find the factors"))
 y = int(input("Enter the second number to find the factors"))
 
+gcf = 1
+
 for i in range(1, min(x, y) + 1):
     if x % i == 0 and y % i == 0:
-        print(i)
+        gcf = i
+    
+print("gcf is", gcf)
